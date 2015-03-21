fabaebf44993b9b96a8f9e7d55cbf3cd4585900f
CrashLearner <yonayen@hotmail.com>
2015-03-21 15:22:49 -0700

    delete scaffold add raty style sign in forms

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,7 @@ class MoviesController < ApplicationController
   # GET /movies/1
   # GET /movies/1.json
   def show
+    @reviews = Review.where(movie_id: @movie_id).order("created_at DESC")
   end
 
   # GET /movies/new
